78e24bde9822eb34016611a1f407b65a242d70cd
luvpame <ayuu.kumakuma@gmail.com>
2026-08-21 15:33:58 +0900

    feat(icons): TextareaVim の拡張機能アイコンを追加

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
     name: 'TextareaVim',
     version: '0.4.0',
     description: 'ブラウザの入力欄をCodeMirror上のVim操作で編集する拡張機能',
+    icons: {
+      '16': 'icons/16.png',
+      '32': 'icons/32.png',
+      '48': 'icons/48.png',
+      '128': 'icons/128.png',
+    },
   },
   vite: function configureVite() {
     return {
